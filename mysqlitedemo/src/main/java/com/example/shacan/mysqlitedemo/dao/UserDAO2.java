3f996b1963cd3cd78f9dfadb8fd73832c22d373c
Shacan <1359854903@qq.com>
2016-08-22 21:19:43 +0800

    封装的SQL功能

--- mysqlitedemo/src/main/java/com/example/shacan/mysqlitedemo/dao/UserDAO2.java
+++ mysqlitedemo/src/main/java/com/example/shacan/mysqlitedemo/dao/UserDAO2.java
@@ -1,6 +1,5 @@
 package com.example.shacan.mysqlitedemo.dao;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -15,20 +14,19 @@ import java.util.List;
 /**
  * Created by Administrator on 2016/8/18 0018.
  */
-public class UserDAO {
+public class UserDAO2 {
     private SQLiteHelper sqLiteHelper;
     private SQLiteDatabase db;
 
-    public UserDAO(Context context) {
+    public UserDAO2(Context context) {
         sqLiteHelper = new SQLiteHelper(context);
     }
 
     public void add(User user) {
         //得到数据库操作对象
         db = sqLiteHelper.getReadableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(Table.USER.NAME,user.getName());
-        db.insert(Table.USER.TABLE_NAME,null,values);
+        String sql = "INSERT INTO " + Table.USER.TABLE_NAME + "(" + Table.USER.NAME + ") VALUES ('" + user.getName() + "')";
+        db.execSQL(sql);
         //关闭数据库
         db.close();
     }
@@ -36,11 +34,8 @@ public class UserDAO {
     public void delete(int id) {
         //得到数据库操作对象
         db = sqLiteHelper.getReadableDatabase();
-        //查询条件
-        String whereClause = Table.USER.ID + "=?";
-        //查询条件所对应的占位符
-        String[] whereArgs = {id+""};
-        db.delete(Table.USER.TABLE_NAME,whereClause,whereArgs);
+        String sql = "DELETE FROM " + Table.USER.TABLE_NAME + " WHERE " + Table.USER.ID + "=" + id;
+        db.execSQL(sql);
         //关闭数据库
         db.close();
 
@@ -49,15 +44,8 @@ public class UserDAO {
     public void upDate(User user) {
         //得到数据库操作对象
         db = sqLiteHelper.getReadableDatabase();
-
-        //更新的内容
-        ContentValues values = new ContentValues();
-        values.put(Table.USER.NAME,user.getName());
-
-        //更新的位置
-        String whereClause = Table.USER.ID +"=?";
-        String[] whereArgs ={user.getId()+""};
-        db.update(Table.USER.TABLE_NAME,values,whereClause,whereArgs);
+        String sql = "UPDATE " + Table.USER.TABLE_NAME + " SET " + Table.USER.NAME + "='" + user.getName() + "' WHERE " + Table.USER.ID + "=" + user.getId();
+        db.execSQL(sql);
         //关闭数据库
         db.close();
 
@@ -67,7 +55,9 @@ public class UserDAO {
         List<User> users = new ArrayList<>();
         //得到数据库操作对象
         db = sqLiteHelper.getReadableDatabase();
-        Cursor cursor = db.query(Table.USER.TABLE_NAME,null,null,null,null,null,null);
+        String sql = "select * from " + Table.USER.TABLE_NAME;
+        String[] selectionArgs = {};
+        Cursor cursor = db.rawQuery(sql, selectionArgs);
         while (cursor.moveToNext()) {
             User user = new User(cursor.getInt(cursor.getColumnIndex(Table.USER.ID)), cursor.getString(cursor.getColumnIndex(Table.USER.NAME)));
             users.add(user);
